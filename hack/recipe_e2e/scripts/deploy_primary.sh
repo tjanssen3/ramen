59a7cc40079083ccb9571598ae8b42f0095b88dc
Travis Janssen <travis.janssen@ibm.com>
2023-02-15 19:18:36 -0800

    Recipe end-to-end scripts for Minikube
    - demonstrates cluster setup and use for basic Recipe with Busybox application
    - requires Velero, MC (Minio Client), and Minikube with KVM2 driver
    - all new files in hack/recipe_e2e
    - see Readme for instructions on usage
    
    Signed-off-by: Travis Janssen <travis.janssen@ibm.com>

---
+++ hack/recipe_e2e/scripts/deploy_primary.sh
@@ -0,0 +1,7 @@
+kubectl apply -f config/ramen_secret_minio.yaml 
+
+kubectl apply -f config/ramen_config.yaml
+
+kubectl apply -f protect/vrg_busybox_primary.yaml
+
+kubectl apply -f protect/recipe_busybox.yaml
